f63e7f86b7d4c4d4a826c4c556c954fb27c9f963
szagr <szagr@softserveinc.com>
2022-06-18 12:35:19 +0300

    Add JsonNode example.

--- src/test/java/com/mailgun/integration/MailgunStoreMessagesIntegrationTest.java
+++ src/test/java/com/mailgun/integration/MailgunStoreMessagesIntegrationTest.java
@@ -1,10 +1,14 @@
 package com.mailgun.integration;
 
+import java.io.IOException;
+import java.util.stream.StreamSupport;
+
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.mailgun.api.v3.MailgunEventsApi;
 import com.mailgun.api.v3.MailgunStoreMessagesApi;
 import com.mailgun.client.MailgunClient;
@@ -12,6 +16,8 @@ import com.mailgun.model.events.EventsQueryOptions;
 import com.mailgun.model.events.EventsResponse;
 import com.mailgun.model.message.MessageResponse;
 import com.mailgun.model.message.StoreMessageResponse;
+import com.mailgun.util.ObjectMapperUtil;
+import feign.Response;
 
 import static com.mailgun.constants.IntegrationTestConstants.EMAIL_TO;
 import static com.mailgun.constants.IntegrationTestConstants.MAIN_DOMAIN;
@@ -60,6 +66,30 @@ class MailgunStoreMessagesIntegrationTest {
 
         assertTrue(StringUtils.isNotEmpty(result.getFrom()));
         assertTrue(StringUtils.isNotEmpty(result.getTo()));
+    }
+
+    @Test
+    void retrieveMessage_FeignResponse_JsonNode_Test() throws IOException {
+        MailgunStoreMessagesApi mailgunStoreMessagesApi = MailgunClient.config(storedMessageUrl, PRIVATE_API_KEY)
+            .createApiWithAbsoluteUrl(MailgunStoreMessagesApi.class);
+
+        Response feignResponse = mailgunStoreMessagesApi.retrieveMessageFeignResponse();
+
+        JsonNode jsonNode = ObjectMapperUtil.decode(feignResponse, JsonNode.class);
+        assertTrue(jsonNode.size() > 10);
+        assertNotNull(jsonNode.get("From"));
+        assertNotNull(jsonNode.get("To"));
+        assertNotNull(jsonNode.get("Message-Id"));
+        JsonNode messageHeaders = jsonNode.get("message-headers");
+        assertTrue(messageHeaders.isArray());
+        Double mimeVersion = StreamSupport.stream(messageHeaders.spliterator(), false)
+            .filter(node -> node.get(0).asText().equals("Mime-Version"))
+            .map(node -> node.get(1))
+            .map(JsonNode::asDouble)
+            .findFirst()
+            .orElse(null);
+        assertNotNull(mimeVersion);
+        assertTrue(mimeVersion >= 1.0);
     }
 
 }
